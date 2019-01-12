98cce44e8b26fcbaa73bd7e44081ec6584fe92ae
xiangkejun <42697766+xiangkejun@users.noreply.github.com>
2019-01-12 16:33:22 +0800

    Update nav_control1.cpp

--- chuan_ws/src/chuan_nav/src/nav_control1.cpp
+++ chuan_ws/src/chuan_nav/src/nav_control1.cpp
@@ -1,5 +1,10 @@
 //xx
-//测试导航到3维激光到图像的控制。
+//测试 定点导航
+// 测试成功
+//测试与图像通信
+//添加 3dlidar
+//nav 定点 与 cv 控制权交接测试
+//20190112
 
 #include "ros/ros.h"
 #include <geometry_msgs/Twist.h>
@@ -23,13 +28,21 @@ ros::Publisher navto3dlidar_flag_pub;
 ros::Subscriber recv_cv_flag_sub;
 ros::Subscriber recv_3dlidar_flag_sub;
 
+
+bool frist = true;
 int flag_cv2nav_status = 0;   //导航标志
 int flag_3dlidar_to_cv_status;  //3d及
 int current_point=0;  //当前导航点
 int current_status=0,last_current_status=0;
-double nav_point[10][4]={
-2.057,0.094,-0.583,0.812,
-1.533,-2.194,-0.999,0.014,
+double nav_point[3][4]={
+//0,0,0,1,     //yuan dian
+// hokuyo 室内的两个点
+3.023,-0.458,0.634,0.773,
+3.164,5.525,0.720,0.694,
+
+// vlp16 导航点 lab out
+// 2.168,0.423,0.035,0.999,
+// 11.377,1.681,0.114,0.993,
 };
 
 int goal_n=0;	
@@ -44,7 +57,9 @@ void set_goal(int i)
 	msg.pose.orientation.z=nav_point[i][2];
 	msg.pose.orientation.w=nav_point[i][3];
 	pub_goal.publish(msg);
-	cout<<"set goal"<<endl;	
+	cout<<"set goal"<<i<<endl;
+    cout<<"goal point at "<<	nav_point[i][0]<<" "<<nav_point[i][1]<<" "
+      <<nav_point[i][2]<< "  "<<nav_point[i][3]<<endl;
 	loop_rate.sleep();
 	goal_n++;
 }
@@ -61,14 +76,6 @@ void pub_switch()
 		fprintf(stderr,"Unsupported stop bits\n");
 	}	
 }
-void send_box_lidar_vel()
-{
-    ros::Rate loop_rate(10);
-    while(ros::ok())
-    {
-        pub_switch();
-    }
-}
 
 
 void do_3d_lidar()
@@ -79,42 +86,33 @@ void do_3d_lidar()
     xx_msgs::Flag flag_nav_to_3dlidar;
     flag_nav_to_3dlidar.flag = "nav stop,3dlidar start";
     navto3dlidar_flag_pub.publish(flag_nav_to_3dlidar);    //导航停止，启动3D激光扫描垃圾
-    while(ros::ok())
-    {
-		//一直等待直到3dbox处理完
-        if(flag_3dlidar_to_cv_status == 1)  //等到3d激光扫描到垃圾且到达垃圾位置附近 退出
-        {
-			flag_3dlidar_to_cv_status = 0;
-            return ;       //退出后，图像接着继续处理
-        }
-    } 
+    // while(ros::ok())
+    // {
+	// //	cout<<"wait 3dlidar flag stop!!"<<endl;
+	// 	//一直等待直到3dbox处理完
+    //     if(flag_3dlidar_to_cv_status == 1)  //等到3d激光扫描到垃圾且到达垃圾位置附近 退出
+    //     {
+	// 	flag_3dlidar_to_cv_status = 0;
+    //        // return ;       //退出后，图像接着继续处理
+	// 	xx_msgs::Flag flag_nav_to_3dlidar;
+	// 	flag_nav_to_3dlidar.flag = "3dbox need stop";
+	// 	navto3dlidar_flag_pub.publish(flag_nav_to_3dlidar);    //导航停止， 停止3dbox和3dlidar
+	// 	cout<<flag_nav_to_3dlidar.flag<<endl;
+	// 		//	break;
+	// 		return ;  //中止当前函数
+    //     }
+    // } 
 }
-
+// 改到在box 处理完后发布
 void do_image()
 {
-    actionlib_msgs::GoalID empty_goal;
-	cancle_pub.publish(empty_goal); //取消导航
+    // actionlib_msgs::GoalID empty_goal;
+	// cancle_pub.publish(empty_goal); //取消导航
 
 	xx_msgs::Flag flag_3dlidar_to_cv;
 	flag_3dlidar_to_cv.flag = "nav stop,cv start";
 	lidartocv_flag_pub.publish(flag_3dlidar_to_cv);   //发布图像控制标志
-	while(ros::ok())
-	{
-		//一直等待 直到有指令返回
-		if(flag_cv2nav_status == 1)   //有标志位返回时 ，开始去下一个点
-		{
-			current_point++;	
-			if(current_point == 2)   //回到起点
-			{
-				current_point = 0;  //原点
-				pub_switch();   
-			}
-	 		pub_switch();  //设置下一个目标点
-			return ;   //退出整个循环
-		}
-	}
 }
-
 
 std::string flag_cv;
 void recv_cv_flag_callback(const xx_msgs::Flag::ConstPtr& msg)  //接收与图像控制权标记
@@ -127,40 +125,91 @@ void recv_cv_flag_callback(const xx_msgs::Flag::ConstPtr& msg)  //接收与图�
 		cout<<"nav do start"<<endl;
 	}
 }
-std::string flag_3dlidar;
-void recv_3dlidar_flag_callback(const xx_msgs::Flag::ConstPtr& msg)
+//std::string flag_3dlidar_to_cv;
+// void recv_3dlidar_flag_callback(const xx_msgs::Flag::ConstPtr& msg)
+// {
+//     flag_3dlidar_to_cv = msg->flag;
+//     cout<< flag_3dlidar_to_cv <<endl;
+//     if(flag_3dlidar_to_cv == "3dbox need stop,cv start")
+//     {
+//         flag_3dlidar_to_cv_status = 1;
+//     }
+// }
+
+void *pub_point(void *arg)  //发布dian
 {
-    flag_3dlidar = msg->flag;
-    cout<< flag_3dlidar <<endl;
-    if(flag_3dlidar == "3dlidar stop,cv start")
-    {
-        flag_3dlidar_to_cv_status = 1;
+    while(ros::ok())
+    {  
+      if(flag_cv2nav_status == 1 )
+	  {
+		flag_cv2nav_status = 0;
+        sleep(1);
+	//	current_point++;
+		if(current_point == 2)   //回到起点
+		{
+			current_point = 0;  //原点
+			pub_switch();   
+		}
+		cout <<"current_point" << current_point<<endl;
+		pub_switch();  //设置下一个目标点
+		current_point++;
+	  }
     }
+}
+
+
+
+void create_all_thread(void)
+{
+
+	pthread_t pub_thread ;
+		
+	if( (pthread_create( &pub_thread , NULL , pub_point , NULL )) != 0 )
+	{
+		perror("Create the thread fail");
+		exit( 1 );
+	}
 }
 
 void statusCallback(const actionlib_msgs::GoalStatusArray::ConstPtr& msg)
 {
+  //  cout <<"goal_n"<<goal_n<<endl;
 	 if (goal_n>0)
 	{	
-		last_current_status=current_status;
-		current_status=msg->status_list[0].status;
-		if ((current_status==3)&&(last_current_status!=current_status))  // 成功到达目的地  SUCCEEDED
+	 	last_current_status=current_status;
+	 	current_status=msg->status_list[0].status;
+    //     cout<<"current_status" << current_status<<endl;
+	 	if ((current_status==3)&&(last_current_status!=current_status))  // 成功到达目的地  SUCCEEDED
+    //    if (current_status==3)
 		{
-			sleep(1);	
-			printf("navigation reach \n");
+				
+			cout<<current_point<<" navigation reach!!"<<endl;
+			sleep(1);
 			switch(current_point)
 			{
-				case 0: do_3d_lidar();do_image(); //到达第一个点
-				case 1: do_3d_lidar();do_image(); //到达第二个点
-                case 2: do_3d_lidar();do_image(); //处理完后回到原点
+				case 0: 
+                 //   do_3d_lidar();  //3dlidar start
+                    do_image(); //到达第一个点
+					cout<<"000000000"<<endl;
+                    break;
+				case 1: 
+                 //   do_3d_lidar();
+                    do_image(); //到达第二个点
+					cout<< "11111111111111"<<endl;
+                    break;
+                case 2: 
+                //    do_3d_lidar();
+                    do_image(); //处理完后回到原点
+					cout<<"222222222"<<endl;
+                    break;
 			}
 		}
-		else if((current_status==4)&&(last_current_status!=current_status))  //目标被流产 ABORTED 状态出现了错误的情况
-		{
-			current_point++;	
-	 		pub_switch();
-	 	}		
-	 }
+		// else if((current_status==4)&&(last_current_status!=current_status))  //目标被流产 ABORTED 状态出现了错误的情况
+		// {
+		// 	current_point++;	
+	 	// 	pub_switch();
+	  	// }		
+	  }
 }
 
 int main(int argc,char** argv)
@@ -173,12 +222,15 @@ int main(int argc,char** argv)
     cancle_pub = n.advertise<actionlib_msgs::GoalID>("move_base/cancel",10);  //用于取消导航
 	sub_status = n.subscribe("move_base/status",10,statusCallback);	//订阅是否到达目标点
 
-    lidartocv_flag_pub = n.advertise<xx_msgs::Flag>("flag_nav_to_cv",1);    //发布控制交接权到图像标记
+  //  lidartocv_flag_pub = n.advertise<xx_msgs::Flag>("flag_nav_to_cv",1);    //发布控制交接权到图像标记
 	navto3dlidar_flag_pub = n.advertise<xx_msgs::Flag>("flag_nav_to_3dlidar",1); //发布控制权交给3d激光
+	
 	recv_cv_flag_sub = n.subscribe<xx_msgs::Flag>("flag_cv_to_nav",1,recv_cv_flag_callback);  //从图像接收控制权标记
-	recv_3dlidar_flag_sub = n.subscribe<xx_msgs::Flag>("flag_3dlidar_to_cv",1,recv_3dlidar_flag_callback); //从3维激光接收标记
+     //从3维激光接收标记用于停止box cv start
+	// recv_3dlidar_flag_sub = n.subscribe<xx_msgs::Flag>("flag_3dlidar_to_cv",1,recv_3dlidar_flag_callback); 
 
-//	create_all_thread();
+
+	create_all_thread();
 	ros::spin();	
     return 0;
 }
