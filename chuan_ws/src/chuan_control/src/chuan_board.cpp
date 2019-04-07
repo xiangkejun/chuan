28acb66927f0d47289752991fc54c28979977938
xiangkejun <42697766+xiangkejun@users.noreply.github.com>
2019-04-07 15:17:38 +0800

    Update chuan_board.cpp

--- chuan_ws/src/chuan_control/src/chuan_board.cpp
+++ chuan_ws/src/chuan_control/src/chuan_board.cpp
@@ -1,61 +1,50 @@
 //xx  20190407
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
 
-
- 
 #include "serial/serial.h"
-
 #include<unistd.h>    //延时  
-
-std::string rec_buf;
-
-int main (int argc,char* argv[])
+float linear_vel=0.0,angular_vel=0.0;
+union floatData  //用于char数组与float之间的转换
 {
-  
- // unsigned char rcv_buf[10];
+  float f;  
+  unsigned char data[4];
+}chaun_linear_vel_send,chuan_angular_vel_send,chaun_linear_vel_rec,chuan_angular_vel_rec;
+
   // 0x0d   "/r" 字符
   // 0x0a   "/n" 字符
-  unsigned char send_buf[10]={0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x0d,0x0a};
+  //{0x0,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x0d,0x0a};
+unsigned char send_buf[10]={0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x0d,0x0a};
 
-  std::string port("/dev/ttyUSB0");
-  unsigned long baud = 115200;
-  serial::Serial my_serial(port,baud,serial::Timeout::simpleTimeout(1000)); //配置串口
+void chuan_vel_callback(const geometry_msgs::Twist::ConstPtr& cmd )
+{
+    std::string port("/dev/ttyUSB0");
+    unsigned long baud = 115200;
+    serial::Serial my_serial(port,baud,serial::Timeout::simpleTimeout(1000)); //配置串口
 
-  while(1) 
-  {
+   // 发送
+      linear_vel = cmd->linear.x;
+      angular_vel = cmd->angular.z;
+      chaun_linear_vel_send.f = linear_vel * 1000; // 放大1000 倍
+      chuan_angular_vel_send.f = angular_vel * 1000;
+      std::cout <<"chaun_linear_vel=" <<chaun_linear_vel_send.f/1000<<std::endl;
+      std::cout<<"chuan_angular_vel="<<chuan_angular_vel_send.f/1000<<std::endl;
+      for(int i=0; i<4; i++)
+      {
+        send_buf[i] = chaun_linear_vel_send.data[i];
+        send_buf[i+4] = chuan_angular_vel_send.data[i];
+      }
       my_serial.write(send_buf,10);
-       sleep(1);  //10ms
+}
 
+int main(int argc, char **argv)
+{
+   ros::init(argc, argv, "chuan_board");
+   ros::NodeHandle n;
+  //  ros::Subscriber sub = n.subscribe("/cmd_vel_mux/input/teleop", 1, keyboard_callback);
+   ros::Subscriber sub = n.subscribe("/mobile_base/commands/velocity", 1, chuan_vel_callback);
 
-     rec_buf = my_serial.readline(10,"\n");
-     const char *receive_data = rec_buf.data();
-     if(rec_buf.length() == 10)
-     {
-          
-      printf("---------%d\n",rec_buf.length());
-      printf("1=%x\n",receive_data[0]);
-      printf("2=%x\n",receive_data[1]);
-		  printf("3=%x\n",receive_data[2]);
-		  printf("4=%x\n",receive_data[3]); 
-      printf("5=%x\n",receive_data[4]);
-      printf("6=%x\n",receive_data[5]);
-		  printf("7=%x\n",receive_data[6]);
-		  printf("8=%x\n",receive_data[7]); 
-      printf("9=%x\n",receive_data[8]); 
-      printf("10=%x\n",receive_data[9]);      
+    ros::spin();
 
-     }
-   //  if(len==4 || rcv_buf[0]== 1 || rcv_buf[3]== 4)	
-   //   {
-
-
-    //  }
-      
-    //   for(i=0;i<3;i++)
-    //  {
-    //    hexbyte[i] = rcv_buf[i];
-    //  }
-    //   Hdecimal=Hex_To_Decimal(hexbyte,sizeof(hexbyte));//十六进制转换为浮点数
-    //   printf("FUFU %f\n",Hdecimal);
-    }
   return 0;
 }
