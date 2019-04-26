a67de81009b4c9e4314a4dd9019e1b8f0b171c60
xiangkejun <42697766+xiangkejun@users.noreply.github.com>
2019-04-26 14:22:47 +0800

    Update auto_nav.sh

--- chuan_nav_ws/auto_nav.sh
+++ chuan_nav_ws/auto_nav.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source /home/nvidia/chuan/chuan_nav_ws/devel/setup.bash
-sleep 2
+source /home/xx/chuan/chuan_nav_ws/devel/setup.bash
+sleep 5
 #roslaunch turtlebot_navigation amcl_chuan.launch
-
-roslaunch chuan_navigation nav_chuan.launch
+#roslaunch chuan_navigation nav_chuan_hokuyo.launch
+roslaunch chuan_navigation nav_chuan_vlp16.launch
