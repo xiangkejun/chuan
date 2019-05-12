2c5bb9f37958e05fb33890e593a4c92f5faf010d
xiangkejun <42697766+xiangkejun@users.noreply.github.com>
2019-05-12 11:16:17 +0000

    Add files via upload

---
+++ sh_xx/hokuyo.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+exit_()
+{
+    exit    
+}
+#检测网络连接
+while true
+do
+ping -c 3 -i 0.2 -W 3 192.168.1.130 &> /dev/null
+#ping -c 3 -i 0.2 -W 3 192.168.1.50 &> /dev/null
+if [ $? -eq 0 ]
+then
+    echo "检测网络正常"
+    break
+else
+    echo "检测网络连接异常"
+fi
+trap exit_ int
+done
+
+
+sleep 5
+source /opt/ros/kinetic/setup.bash
+source  ~/.bashrc
+export ROS_MASTER_URI=http://control:11311
+export ROS_HOSTNAME=control
+
+source /home/nvidia/chuan/chuan_nav_ws/devel/setup.bash
+roslaunch hokuyo_node hokuyo_laser.launch 
+
+
