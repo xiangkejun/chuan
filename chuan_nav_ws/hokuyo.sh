48bdbac0ff8a51e20a4b6fe9daeb8ceada3da51a
xiangkejun <42697766+xiangkejun@users.noreply.github.com>
2019-05-12 11:15:43 +0000

    Add files via upload

---
+++ chuan_nav_ws/hokuyo.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+sleep 1
+source /home/nvidia/chuan/chuan_nav_ws/devel/setup.bash
+roslaunch hokuyo_node hokuyo_laser.launch 
+
+
---
+++ chuan_nav_ws/hokuyo_rviz.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+sleep 1
+source /home/nvidia/chuan/chuan_nav_ws/devel/setup.bash
+
+roslaunch hokuyo_node hokuyo_rviz.launch 
